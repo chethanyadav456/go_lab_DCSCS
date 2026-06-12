33630dc7443ec15cc2e713ed43d0b99fb222e874
chethanyadav456 <chethanyadav456@gmail.com>
2026-06-12 10:02:30 +0530

    added notes

--- hello.go
+++ hello.go
@@ -1,6 +1,47 @@
+/* 
+Go is a cross-platform, open source programming language
+Go can be used to create high-performance applications
+Go is a fast, statically typed, compiled language known for its simplicity and efficiency
+Go was developed at Google by Robert Griesemer, Rob Pike, and Ken Thompson in 2007
+Go's syntax is similar to C++
+*/
+
 package main
 import ("fmt")
 
 func main() {
 	fmt.Println("Hello, Go!")
 }
+
+/*
+- Module 1 - Introduction to Go
+
+Go is an open-source programming language that makes it easy to build simple, reliable, and efficient software. It is an open-source programming language developed by engineers at Google in 2007 and officially released in 2009. Go Lang was designed by Robert Griesemer, Rob Pike, and Ken Thompson while there were working at Google. It was was designed for :-
+- Simple
+- Fast 
+- Efficient
+- Concurrent
+- Easy to learn
+
+-> features of Go Lang
+
+- open-source
+- compiled language
+- Strongly and statically typed
+- garbage collected
+- built-in support for concurrent programming
+- Cross-platform Support
+- Fast Execution
+
+-> Applications of Go Lang
+
+Go is widely used for :-
+- web development
+- cloud computing
+- networking services
+- devOps tools
+- microservices
+- CLI tools
+- Real-Time Applications
+
+*/
